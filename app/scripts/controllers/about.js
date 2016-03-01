e503b93f3e291b77b136437341a9a2b9537ecc4e
jimacampos <jimmycampos@Jimmys-MacBook-Pro.local>
2016-02-29 23:58:28 -0600

    Initial Project
    Add generated angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name checkinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the checkinApp
+ */
+angular.module('checkinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name checkinApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the checkinApp
+ */
+angular.module('checkinApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
